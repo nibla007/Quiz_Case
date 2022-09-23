c9121c84f8d960240de06f2cf82aa74d52dba2ce
Albin <nibla007@hotmail.com>
2022-09-23 11:48:44 +0200

    First commit

---
+++ questions.py
@@ -0,0 +1,30 @@
+quiz = {
+    '1': {
+        "question": "What function is used to write things to the screen?",
+        "answer": "print"
+    },
+    '2': {
+        "question": "Is python case sensitive?",
+        "answer": "yes"
+    },
+    '3': {
+        "question": "2",
+        "answer": "2"
+    },
+    '4': {
+        "question": "2",
+        "answer": "2"
+    },
+    '5': {
+        "question": "",
+        "answer": ""
+    },
+    '6': {
+        "question": "",
+        "answer": ""
+    },
+    '7': {
+        "question": "",
+        "answer": ""
+    },
+}
